a0c7e1b963f0fdc363595f09d184ad79f63d7dd1
Scott-Zeta <wc070211@gmail.com>
2023-02-02 13:02:26 +1030

    secret generator

--- Assignment1/q05_cyber_key.py
+++ Assignment1/q05_cyber_key.py
@@ -11,15 +11,18 @@ def main():
     inputsr = sys.argv[1]
     l1 = len(inputsr)
     l2 = 100
+    f = open("secret.txt","w")
     for c in inputsr:
         p = random.randint(0,l2)
         salt1 = randomString(p)
         salt2 = randomString(l2 - p)
         x = '{:04d}'.format(p) + ":" + salt1 + c + salt2
         print(x)
+        f.write(x + "\n")
         print(salt1)
         print(c)
         print(salt2)
+    f.close()
 
 if __name__ == "__main__":
     main()
