b96ea22e504d0e439c33307b9c8524934dec6cad
Scott-Zeta <wc070211@gmail.com>
2023-02-09 14:42:45 +1030

    q_13 initial

--- Assignment1/q12_cracker.py
+++ Assignment1/q12_cracker.py
@@ -21,8 +21,23 @@ def freq():
             dic[c] = dic.get(c,0)+1    
     print(dic)
     
+def subsitution():
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cipherbet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    dic = {}
+    for n in range(len(alphabet)):
+        dic[alphabet[n]] = cipherbet[n]
+    print(dic)
+    with open("q12_ciphertext.txt") as f:
+        s = f.read()
+    new_s=""
+    for c in s:
+        c = dic.get(c,c)
+        new_s += c
+    print(new_s)
+    
 def main():
-    freq()
+    subsitution()
     
 if __name__ == "__main__":
     main()
