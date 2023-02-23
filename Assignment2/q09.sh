80af8bda1ae2ee0599574b071a1bcbe1325ae207
Scott-Zeta <wc070211@gmail.com>
2023-02-23 11:02:08 +1030

    q09 can 100% get the answer

--- Assignment2/q09.sh
+++ Assignment2/q09.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo nmap -Pn -sS -p 1122,2233,3344 192.168.137.128
+sudo nmap -Pn -sS -r -n -p 1122,2233,3344 192.168.137.128
 netcat -v -w 15 192.168.137.128 12345
+
+# -r will follow the sequence of the port, or it would send package randomly
--- Assignment2/q09_compare.sh
+++ Assignment2/q09_compare.sh
@@ -4,3 +4,5 @@ sudo nmap -Pn -sS -p 2233 192.168.137.128
 sudo nmap -Pn -sS -p 3344 192.168.137.128
 
 netcat -v -w 15 192.168.137.128 12345
+
+# This is invalid
